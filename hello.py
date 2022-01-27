98a6a97e66c37afd1fd609d848de0ce73e25b861
Kim-YH-handong <82792998+Kim-YH-handong@users.noreply.github.com>
2022-01-27 18:27:35 +0900

    helloworld

---
+++ hello.py
@@ -0,0 +1,7 @@
+kor = int(input("국어점수:"))
+eng = int(input("영어점수:"))
+math = int(input("수학점수:"))
+total = kor + eng + math
+average = total/3.0
+
+print("입력하신 점수의 총점은", total, "이고,\n 평균은", average, "입니다.")
